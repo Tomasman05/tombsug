969014f8cbc55aedde6af8e23ce52661a7d6eaa6
Vito <lolozol80xd@gmail.com>
2024-01-09 11:48:35 +0100

    teszt6

--- src/test/java/TombsugTest.java
+++ src/test/java/TombsugTest.java
@@ -12,6 +12,6 @@ public class TombsugTest {
 
     @Test
     public void testCalcRad_30_35(){
-        this.tombsug.calcRadius(30,35);
+        double actual = this.tombsug.calcRadius(30,35);
     }
 }
