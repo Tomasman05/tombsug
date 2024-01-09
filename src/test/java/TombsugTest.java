57a4a186ccc410ea046b4f700e7b1269a3fc2d4f
Vito <lolozol80xd@gmail.com>
2024-01-09 11:43:39 +0100

    teszt3

--- src/test/java/TombsugTest.java
+++ src/test/java/TombsugTest.java
@@ -1,11 +1,17 @@
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import lan.zold.Tombsug;
 
 public class TombsugTest {
     Tombsug tombsug;
+    @BeforeTest
+    public void setUp(){
+        this.tombsug= new Tombsug();
+    }
+
     @Test
     public void testCalcRad_30_35(){
-
+        this.tombsug.calRadius();
     }
 }
